85e90be31a82e31e77da75bca115e259e70db83c
Elliot Tio <etjinzo@gmail.com>
2018-04-02 17:01:56 -0400

    fixed bug where 'pick' option would show results

--- Assignment 2 - Iteration 2/includes/_dropdowns.php
+++ Assignment 2 - Iteration 2/includes/_dropdowns.php
@@ -4,7 +4,7 @@ echo '<ul class="nav-bar">
         <li>
           <form action="artworks.php" method="post">
             <select id="artworks" name="selected_artwork">
-              <option value="pick">Pick an Artwork</option>
+              <option value="">Pick an Artwork</option>
               <option value="Mona Lisa" name="Mona Lisa">Mona Lisa</option>
               <option value="De Matteis Triumph of the Immaculate Anagoria">De Matteis Triumph of the Immaculate Anagoria</option>
               <option value="The Delivery of the Keys to Saint Peter">The Delivery of the Keys to Saint Peter</option>
@@ -18,7 +18,7 @@ echo '<ul class="nav-bar">
         <li>
           <form action="artists.php" method="post">
             <select id="artists" name="selected_artist">
-              <option value="pick">Pick an Artist</option>
+              <option value="">Pick an Artist</option>
               <option value="Leonardo da Vinci" id="1">Leonardo da Vinci</option>
               <option value="Vincent van Gogh">Vincent van Gogh</option>
               <option value="Michelangelo">Michelangelo</option>
@@ -32,7 +32,7 @@ echo '<ul class="nav-bar">
         <li>
           <form action="museums.php" method="post">
           <select id="museums" name="selected_museum">
-            <option value="pick">Pick an Museum</option>
+            <option value="">Pick an Museum</option>
             <option value="Stoa of Attalos">Stoa of Attalos</option>
             <option value="Louvre">Louvre</option>
             <option value="Royal Ontario Museum">Royal Ontario Museum</option>
