21c42994639e873edb8aae4d5310aa18bdab2534
Elliot Tio <etjinzo@gmail.com>
2018-04-05 00:10:22 -0400

    functional search and maintain on moon

--- iteration2/includes/_navbar.php
+++ iteration2/includes/_navbar.php
@@ -6,7 +6,7 @@ echo '<div class="topnav">
         <a href="#" id="amodal" onmousedown="myModal()"><img src=https://image.flaticon.com/icons/png/512/34/34627.png width="25" height="25"></a>
         <a href="#" class="pull-right" onmousedown="toggleBar()">Search</a>
         <div id="bar" class="search-bar" style="display:none;">
-    			<form action= "searchbar.php" method="post">
+    			<form action= "results.php" method="post">
     				<input type="text" placeholder="Search.." name="in_search" autofocus>
     				<button type="submit" name="search"><img src="http://worldartsme.com/images/search-button-clipart-1.jpg" width=20; height=20;></button>
     			</form>
